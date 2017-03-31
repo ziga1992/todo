aeb24bf93a9c9e2dc5292421f5016ba7cb1cf3af
Ziga Kralj <ziga1992@gmail.com>
2017-03-31 19:27:33 +0200

    dodano branje prioritet iz BE in zgeneriranje v HTML meni

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var tags = [];
     // var host = "http://localhost:52001/";
     var host = "http://89.142.196.244:52001/";
-
+    // Funcija, ki doda opravek
     $("#add").click(function() {
         var todoTitle = $("#todoTitle").val();
         var todoDescription = $("#todoDescription").val();
@@ -38,61 +38,12 @@ $(document).ready(function() {
             }
         });
     });
+    // Date-picker
     $('.date-datepicker').datepicker({
         format: "yyyy/mm/dd",
         autoclose: true
     });
-    $(".menu-high").click(function() {
-        if($(this).hasClass("current-priority")) {
-            $(this).removeClass("current-priority");
-        } else {
-            $(".current-priority").removeClass("current-priority");
-            $(this).addClass("current-priority");
-        }
-        filterTodos();
-    });
-    $(".menu-medium").click(function() {
-         if($(this).hasClass("current-priority")) {
-            $(this).removeClass("current-priority")
-        } else {
-            $(".current-priority").removeClass("current-priority");
-            $(this).addClass("current-priority");
-        }
-        filterTodos();
-    });
-    $(".menu-low").click(function() {
-         if($(this).hasClass("current-priority")) {
-            $(this).removeClass("current-priority")
-        } else {
-            $(".current-priority").removeClass("current-priority");
-            $(this).addClass("current-priority");
-        }
-        filterTodos();
-    });
-    $(".menu-personal").click(function() {
-         if($(this).hasClass("current-tag")) {
-            $(this).removeClass("current-tag")
-        } else {
-            $(this).addClass("current-tag");
-        }
-        filterTodos();
-    });
-    $(".menu-work").click(function() {
-         if($(this).hasClass("current-tag")) {
-            $(this).removeClass("current-tag")
-        } else {
-            $(this).addClass("current-tag");
-        }
-        filterTodos();
-    });
-    $(".menu-shopping").click(function() {
-         if($(this).hasClass("current-tag")) {
-            $(this).removeClass("current-tag")
-        } else {
-            $(this).addClass("current-tag");
-        }
-        filterTodos();
-    });
+    // Funkcija za odstranitev opravka
     $("#remove").click(function() {
         $( ".todo-checkbox:checkbox:checked" ).each(function( index ) {
             var id = $(this).parents(".task").data("id");
@@ -113,6 +64,7 @@ $(document).ready(function() {
             });
         });
     });
+    // BackEnd pull
     $.get( host + "todos", function(data) {
         todos = data;
         console.log("todos", todos);
@@ -124,29 +76,19 @@ $(document).ready(function() {
         prios = data;
         console.log("prios", prios);
         for(var prio of prios) {
-            addPriority(prio);
             addPriorityToSelect(prio);
+            addPriorityMenu(prio);
         }
     });
     $.get( host + "tags", function(data) {
         tags = data;
         console.log("tags", tags);
         for(var tag of tags) {
-            addTag(tag);
+            addTagMenu(tag);
         }
     });
 });
 
-// Funkcija, ki doda oznako
-function addTag(oznaka) {
-    var tag = $('<div class="col-lg-12"><p style="border-left: 5px solid ' + oznaka.color + '">' + oznaka.name + '</p></div>')
-    $("#tag").append(tag);
-}
-// Funkcija, ki doda prioriteto
-function addPriority(prioriteta) {
-    var prio = $('<div class="col-lg-12"><p>' + prioriteta.name + '</p></div>')
-    $("#prio").append(prio);
-}
 // Funkcija, ki doda prioriteto v opravek
 function addPriorityToSelect(prioriteta) {
     var prio = $('<option value="' + prioriteta.id + '">' + prioriteta.name + '</option>')
@@ -181,6 +123,34 @@ function addTodo(opravek) {
         });
     });
 }
+// Funkcija, ki doda tag v menu
+function addTagMenu(tag) {
+    var tags = generateTagMenu(tag);
+    $(".tag-menu").append(tags);
+    tags.click(function() {
+         if($(this).hasClass("current-tag")) {
+            $(this).removeClass("current-tag")
+        } else {
+            $(this).addClass("current-tag");
+        }
+        filterTodos();
+    });
+}
+// Funkcija, ki doda prioritete v menu
+function addPriorityMenu(priority) {
+    var prio = generatePriorityMenu(priority);
+    $(".prio-menu").append(prio);
+    prio.click(function() {
+         if($(this).hasClass("current-priority")) {
+            $(this).removeClass("current-priority")
+        } else {
+            $(".current-priority").removeClass("current-priority");
+            $(this).addClass("current-priority");
+        }
+        filterTodos();
+    });
+}
+// Fukcija, ki zgenerira HTML opraveka
 function generateTodoHtml(opravek) {
     var done = "";
     if(opravek.checked == "true") {
@@ -198,6 +168,19 @@ function generateTodoHtml(opravek) {
 
     return $(html);
 }
+// Funkcija, ki zgenerira HTML oznake
+function generateTagMenu(tag) {
+    var html = '<div class="menu"><i class="fa ' + tag.icon + '" data-id="' + tag.id + '" aria-hidden="true" style="color:' + tag.color + '"></i><div>' + tag.name + '</div></div>';
+
+    return $(html);
+}
+// Funkcija, ki zgenerira HTML prioritete
+function generatePriorityMenu(priority) {
+    var html = '<div class="menu"><i class="fa ' + priority.icon + '" data-id="' + priority.id + '" aria-hidden="true" style="color:' + priority.color + '"></i><div style="color:' + priority.color + '">' + priority.name + '</div></div>';
+
+    return $(html);
+}
+// Funcija, ki pokaze prioritete
 function showPriorities(priority) {
     $(".task").each(function() {
         if($(this).data("priority") == priority) {
@@ -207,6 +190,7 @@ function showPriorities(priority) {
         };
     });
 }
+// Funcija, ki filtrira opravke
 function filterTodos() {
     var selected_priority = $(".current-priority").find("div").text();
     var selected_tags = [];
